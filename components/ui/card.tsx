ca4a1b88a4415fe7c11d380bc82a6a8de46b57c7
dzodze00 <pakpablie@gmail.com>
2025-05-13 15:58:43 -0700

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,9 +1,8 @@
 import * as React from "react"
-
 import { cn } from "@/lib/utils"
 
 const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)} {...props} />
+  <div ref={ref} className={cn("rounded-lg border bg-white text-gray-950 shadow-sm", className)} {...props} />
 ))
 Card.displayName = "Card"
 
@@ -22,9 +21,7 @@ const CardTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HT
 CardTitle.displayName = "CardTitle"
 
 const CardDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLParagraphElement>>(
-  ({ className, ...props }, ref) => (
-    <p ref={ref} className={cn("text-sm text-muted-foreground", className)} {...props} />
-  ),
+  ({ className, ...props }, ref) => <p ref={ref} className={cn("text-sm text-gray-500", className)} {...props} />,
 )
 CardDescription.displayName = "CardDescription"
 
